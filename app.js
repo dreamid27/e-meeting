57f44aec10d61e747a6a73843d0b6b230402d3e0
Muhammad Abdulmajid Alfaris <abdulmajid.alfaris@bridestory.com>
2019-12-08 16:48:36 +0700

    add methods alloed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "*");
     next();
     
 });
